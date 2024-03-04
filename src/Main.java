05d6a17dab7bf412135e1cb63352273020aa82a8
Soprodevento <Sashinka2803@gmail.com>
2024-03-04 20:05:27 +0300

    Исправил

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        byte result = getAgeDiff((byte) 5,(byte) 3);
+        byte result = getAgeDiff((byte) 50,(byte) 100);
         System.out.println(result);
     }
 
     public static byte getAgeDiff(byte age1, byte age2) {
-        return (byte) (age1 - age2);
+        return (byte) Math.abs (age1 - age2);
     }
 }
